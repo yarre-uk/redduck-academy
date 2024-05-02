91eeb6c22669138ea3f82ec08d48615bbf3730ac
Yarre <yaroslav.syvukha@gmail.com>
2024-05-02 15:29:27 +0200

    feat: 3.3

--- test/Vesting.ts
+++ test/Vesting.ts
@@ -1,10 +1,12 @@
 import { loadFixture, time } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import keccak256 from "keccak256";
 
 import {
   Vesting1__factory,
   Vesting2__factory,
+  Vesting3__factory,
   YarreToken__factory,
 } from "../typechain-types";
 import MerkleTree from "../utils/merkle";
@@ -13,10 +15,10 @@ const ACCOUNT_NUMBER = 150;
 
 describe("Vesting1", async () => {
   async function deploy() {
-    const [owner] = await ethers.getSigners();
+    const [owner, acc1, acc2] = await ethers.getSigners();
 
     const tokenContract = await new YarreToken__factory(owner).deploy(
-      10000n * BigInt(ACCOUNT_NUMBER) * 2n,
+      10000n * BigInt(ACCOUNT_NUMBER) * 3n,
       10,
     );
 
@@ -25,6 +27,10 @@ describe("Vesting1", async () => {
     );
 
     const vestingContract2 = await new Vesting2__factory(owner).deploy(
+      tokenContract.getAddress(),
+    );
+
+    const vestingContract3 = await new Vesting3__factory(owner).deploy(
       tokenContract.getAddress(),
     );
 
@@ -38,11 +44,19 @@ describe("Vesting1", async () => {
       10000n * BigInt(ACCOUNT_NUMBER),
     );
 
+    await tokenContract.transfer(
+      vestingContract3.getAddress(),
+      10000n * BigInt(ACCOUNT_NUMBER),
+    );
+
     return {
       tokenContract,
       vestingContract1,
       vestingContract2,
+      vestingContract3,
       owner,
+      acc1,
+      acc2,
     };
   }
 
@@ -76,10 +90,6 @@ describe("Vesting1", async () => {
       const signer = vestingContract1.connect(account);
       await signer.claim();
       expect(await tokenContract.balanceOf(account.address)).to.equal(10000n);
-
-      if (i % 10 === 0) {
-        console.log(i);
-      }
     }
   }).timeout(100000000);
 
@@ -118,10 +128,50 @@ describe("Vesting1", async () => {
       const proof = tree.getMerkleProof(i);
       await signer.claim(proof);
       expect(await tokenContract.balanceOf(account.address)).to.equal(10000n);
-
-      if (i % 10 === 0) {
-        console.log(i);
-      }
     }
+  }).timeout(100000000);
+
+  it("should work Signature", async () => {
+    const { tokenContract, vestingContract3, owner, acc1, acc2 } =
+      await loadFixture(deploy);
+
+    let nonce = 0;
+
+    const signMessage = async (
+      to: string,
+      amount: number,
+      contractAddress: string,
+    ) => {
+      const hash = keccak256(
+        ethers.solidityPacked(
+          ["address", "uint256", "uint256", "address"],
+          [to, amount, nonce++, contractAddress],
+        ),
+      );
+
+      return owner.signMessage(hash);
+    };
+
+    const sig1 = await signMessage(
+      acc1.address,
+      10000,
+      await vestingContract3.getAddress(),
+    );
+
+    const sig2 = await signMessage(
+      acc2.address,
+      5000,
+      await vestingContract3.getAddress(),
+    );
+
+    await time.increase(60 * 60 * 24 * 365 * 2);
+
+    await vestingContract3.connect(acc1).claim(10000, 0, sig1);
+
+    expect(await tokenContract.balanceOf(acc1.address)).to.equal(10000);
+
+    await vestingContract3.connect(acc2).claim(5000, 1, sig2);
+
+    expect(await tokenContract.balanceOf(acc2.address)).to.equal(5000);
   }).timeout(100000000);
 });
